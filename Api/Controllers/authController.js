9c55ec08dbac1afc73d8778c55473f7d3dae0376
Pandit Dhamdhere <dhamdherepandit@gmail.com>
2024-01-02 14:44:03 +0530

    auth controller modal done

--- Api/Controllers/authController.js
+++ Api/Controllers/authController.js
@@ -2,29 +2,84 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
 const signToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN
-    })
-}
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 
 const createSendToken = (user, statusCode, req, res) => {
-    const token = signToken(user._id);
+  const token = signToken(user._id);
 
-    res.cookie("jwt", token, {
-        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
-        httpOnly: true,
-        secure: req.secure || req.headers["x-forwarded-proto"] === "https",
-    })
+  res.cookie("jwt", token, {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    secure: req.secure || req.headers["x-forwarded-proto"] === "https",
+  });
 
-    // Remove password from output
-    user.password = undefined;
+  // Remove password from output
+  user.password = undefined;
 
-    // Continue with the rest of the code
-    res.status(statusCode).json({
-        status: "success",
-        token,
-        data: {
-            user
-        }
-    })
-}
+  // Continue with the rest of the code
+  res.status(statusCode).json({
+    status: "success",
+    token,
+    data: {
+      user,
+    },
+  });
+};
+
+exports.signUp = async (req, res, next) => {
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+  createSendToken(newUser, 201, req, res);
+};
+
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // 1) Check if email and password exist
+  if (!email || !password) {
+    res.status(400).json({
+      status: "fail",
+      message: "Please provide email and password",
+    });
+  }
+
+  // 2) Check if user exists && password is correct
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    res.status(401).json({
+      status: "fail",
+      message: "Incorrect email or password",
+    });
+  }
+
+  createSendToken(user, 200, req, res);
+};
+
+exports.buyMembership = async (req, res, next) => {
+  const updatedUser = await User.findByIdAndUpdate(
+    req.body.userID,
+    {
+      membershipType: req.body.membershipType,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  res.status(200).json({
+    title: "Your account",
+    user: updatedUser,
+  });
+};
