fab2f327e572316eac0eb8d62ca4c5677a56bed9
Pandit Dhamdhere <dhamdherepandit@gmail.com>
2024-01-03 13:09:37 +0530

    imported and exported all components

---
+++ components/Global/MovingSubmenu.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MovingSubmenu = () => {
+  return (
+    <div>MovingSubmenu</div>
+  )
+}
+
+export default MovingSubmenu
---
+++ components/index.js
@@ -0,0 +1,50 @@
+// Import Global components
+
+import Footer from "./Global/Footer";
+import Header from "./Global/Header";
+import Login from "./Global/Login";
+import Signup from "./Global/Signup";
+import Loader from "./Global/Loader";
+import Preloader from "./Global/Preloader";
+import Search from "./Global/Search";
+import SideBar from "./Global/SideBar";
+import MovingSubmenu from "./Global/MovingSubmenu";
+import useTimeout from "./Global/useTimeout";
+
+// body components
+
+import AddNetwork from "./Body/AddNetwork";
+import AddTokenPair from "./Body/AddTokenPair";
+import Home from "./Body/Home";
+import Networks from "./Body/Networks";
+import Price from "./Body/Price";
+import Profile from "./Body/Profile";
+import Setting from "./Body/Setting";
+import TopExchangeTokens from "./Body/TopExchangeTokens";
+import Trading from "./Body/Trading";
+import TradeTokens from "./Body/TradeTokens";
+
+// exports
+
+export {
+  Footer,
+  Header,
+  Login,
+  Signup,
+  Loader,
+  Preloader,
+  Search,
+  SideBar,
+  MovingSubmenu,
+  useTimeout,
+  AddNetwork,
+  AddTokenPair,
+  Home,
+  Networks,
+  Price,
+  Profile,
+  Setting,
+  TopExchangeTokens,
+  Trading,
+  TradeTokens,
+};
